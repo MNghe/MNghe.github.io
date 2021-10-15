1f8e0dad3c4a62335634c485d9902b26d4e8a695
MNghe <61427162+MNghe@users.noreply.github.com>
2021-10-15 00:19:51 -0400

    AWS Hello World Draft
    Added a separate .js file for navigation on the AWS projects' pages. Added the hyperlink of the tutorial on Hello, World, and started adding content onto the said page.

---
+++ AWS-Project-Pages/scripts.js
@@ -0,0 +1,14 @@
+document.write('\
+<link rel="stylesheet" href="../navigator.css">\
+  <div class="dropdown">\
+    <img src="../Home Images/balloonblueboi.png" alt="Navigation Menu"\
+      style="height: auto; transform: scale(0.5); margin-left: 3vw;">\
+    <div class="dropdown-content">\
+      <a href="../index.html">Home</a>\
+      <a href="../HowWebsite.html">How I Made This</a>\
+      <a href="../AWSprojects.html">AWS Projects</a>\
+      <a href="../TempReading.html">Logging Sensor Data</a>\
+      <a href="../RGB.html">RGB Filter</a>\
+    </div>\
+  </div>\
+');
